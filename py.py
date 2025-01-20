523e8692cebb50c6db7063c77e9329b77ce20531
Raj Patel <raj721patel@gmail.com>
2025-01-20 14:12:23 -0600

    Added class

--- py.py
+++ py.py
@@ -3,4 +3,18 @@ import requests
 from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout) 
 from PyQt5.QtCore import Qt
 
-print("Hello")
+class Weatherapp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.city_label = QLabel("Enter city name: ", self)
+        self.city_input = QLineEdit(self)
+        self.get_weather_button = QPushButton("Get Weather", self)
+        self.temperature_label = QLabel("65°F", self)
+        self.emoji_label = QLabel("😎", self)
+        self.description_label = QLabel("Sunny", self)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    weather_app = Weatherapp()
+    weather_app.show()
+    sys.exit(app.exec_())
